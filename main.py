3cf3c1429c94a1adf0c911497c346b8696fb1898
Anil-matcha <Vadoogit@gmail.com>
2023-05-09 20:35:45 +0530

    ChatPDF first code commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+
+# Replace book.pdf with any pdf of your choice
+loader = UnstructuredPDFLoader("book.pdf")
+pages = loader.load_and_split()
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+
+# Choose any query of your choice
+query = "Who is Rich Dad?"
+docs = docsearch.get_relevant_documents(query)
+chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+output = chain.run(input_documents=docs, question=query)
+print(output)
